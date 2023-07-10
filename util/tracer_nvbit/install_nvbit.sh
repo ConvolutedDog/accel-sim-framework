65818e7c10678fa71b02c0acdb5ffc10f9e0a052
masa-laboratory <masa-lab@outlook.com>
2023-07-10 20:41:19 +0800

    Update install_nvbit.sh

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -1,7 +1,7 @@
 export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 
 rm -rf $BASH_ROOT/nvbit_release
-#wget https://github.com/NVlabs/NVBit/releases/download/1.5.5/nvbit-Linux-x86_64-1.5.5.tar.bz2
+#wget https://hub.nuaa.cf/NVlabs/NVBit/releases/download/1.5.5/nvbit-Linux-x86_64-1.5.5.tar.bz2
 cp /home/masa/Downloads/nvbit-Linux-x86_64-1.5.5.tar.bz2 ./
 tar -xf nvbit-Linux-x86_64-1.5.5.tar.bz2 -C $BASH_ROOT
 rm nvbit-Linux-x86_64-1.5.5.tar.bz2
